f22f837288826714be87218f47ce13271cb1c4de
alisariaslann <125967344+alisariaslann@users.noreply.github.com>
2026-02-02 06:40:14 +0300

    feat: add logging command and handler; update .gitignore

---
+++ FullStackTask.Microservices/Services.Log.Application/Features/Logs/Commands/CreateLog/CreateLogCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Services.Log.Application.Features.Logs.Commands.CreateLog
+{
+    public class CreateLogCommand : IRequest<bool>
+    {
+        public required string ServiceName { get; set; }
+        public required string Message { get; set; }
+        public required string Level { get; set; }
+        public string? StackTrace { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
